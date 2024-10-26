221ce807e543153feca5557f73629af1fcb40239
divykirtisrivastava <divykirtisrivastava@gmail.com>
2024-10-26 11:34:49 +0530

    added

--- filixobackend/controllers/profileController.js
+++ filixobackend/controllers/profileController.js
@@ -369,24 +369,6 @@ exports.getcontact = (req, res)=>{
   })
 }
 
-const mysql = require('mysql');
-
-// Create a connection to the database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'your_database_name'
-});
-
-// Connect to the database
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-    return;
-  }
-  console.log('Connected to the MySQL database.');
-});
 
 exports.saveSumOfDeposits = (req, res) => {
   const spsemail = req.params.sponsorEmail;
